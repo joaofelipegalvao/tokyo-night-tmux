2f822f1901992d16dafe5e0d874b9e5252765702
joaofelipegalvao <176946005+joaofelipegalvao@users.noreply.github.com>
2025-09-29 23:58:13 -0300

    style(tmux): improve status bar layout with powerline separators

--- src/tokyonight-status-content-no-patched-font.sh
+++ src/tokyonight-status-content-no-patched-font.sh
@@ -21,16 +21,18 @@ TIME_FORMAT=${TIME_FORMAT:-"%H:%M"}
 #+ Plugin Support +
 #+----------------+
 #+--- tmux-prefix-highlight ---+
+tmux set -g @prefix_highlight_output_prefix "#[fg=${PALLETE[magenta]}]#[bg=${PALLETE[bg]}]#[nobold]#[noitalics]#[nounderscore]#[bg=${PALLETE[magenta]}]#[fg=${PALLETE[bg]}]"
+tmux set -g @prefix_highlight_output_suffix ""
 tmux set -g @prefix_highlight_copy_mode_attr "fg=${PALLETE[bg]},bg=${PALLETE[magenta]}"
 
 #+--------+
 #+ Status +
 #+--------+
 #+--- Bars ---+
-tmux set -g status-left "#[fg=${PALLETE[bg]},bg=${PALLETE[magenta]},bold] #S "
-tmux set -g status-right "#{prefix_highlight}#[fg=${PALLETE[fg]},bg=${PALLETE[bg_highlight]}] ${DATE_FORMAT} #[fg=${PALLETE[fg]},bg=${PALLETE[bg_highlight]},nobold,noitalics,nounderscore]|#[fg=${PALLETE[fg]},bg=${PALLETE[bg_highlight]}] ${TIME_FORMAT} #[fg=${PALLETE[magenta]},bg=${PALLETE[bg_highlight]},nobold,noitalics,nounderscore] #[fg=${PALLETE[bg]},bg=${PALLETE[magenta]},bold] #H "
+tmux set -g status-left "#[fg=${PALLETE[bg]},bg=${PALLETE[green]},bold] #S #[fg=${PALLETE[green]},bg=${PALLETE[bg]},nobold,noitalics,nounderscore]"
+tmux set -g status-right "#{prefix_highlight}#[fg=${PALLETE[bg_highlight]},bg=${PALLETE[bg]},nobold,noitalics,nounderscore]#[fg=${PALLETE[fg]},bg=${PALLETE[bg_highlight]}] ${DATE_FORMAT} #[fg=${PALLETE[fg]},bg=${PALLETE[bg_highlight]},nobold,noitalics,nounderscore]#[fg=${PALLETE[fg]},bg=${PALLETE[bg_highlight]}] 󰥔 ${TIME_FORMAT} #[fg=${PALLETE[magenta]},bg=${PALLETE[bg_highlight]},nobold,noitalics,nounderscore]#[fg=${PALLETE[bg]},bg=${PALLETE[magenta]},bold] #H "
 
 #+--- Windows ---+
-tmux set -g window-status-format " #[fg=${PALLETE[fg]},bg=${PALLETE[bg_highlight]}]#I #[fg=${PALLETE[fg]},bg=${PALLETE[bg_highlight]}]#W #F"
-tmux set -g window-status-current-format " #[fg=${PALLETE[bg]},bg=${PALLETE[magenta]}]#I#[fg=${PALLETE[bg]},bg=${PALLETE[magenta]},nobold,noitalics,nounderscore] #[fg=${PALLETE[bg]},bg=${PALLETE[magenta]}]#W #F"
+tmux set -g window-status-format "#[fg=${PALLETE[bg]},bg=${PALLETE[bg_highlight]},nobold,noitalics,nounderscore] #[fg=${PALLETE[fg]},bg=${PALLETE[bg_highlight]}]#I #[fg=${PALLETE[fg]},bg=${PALLETE[bg_highlight]},nobold,noitalics,nounderscore] #[fg=${PALLETE[fg]},bg=${PALLETE[bg_highlight]}]#W #F #[fg=${PALLETE[bg_highlight]},bg=${PALLETE[bg]},nobold,noitalics,nounderscore]"
+tmux set -g window-status-current-format "#[fg=${PALLETE[bg]},bg=${PALLETE[magenta]},nobold,noitalics,nounderscore] #[fg=${PALLETE[bg]},bg=${PALLETE[magenta]}]#I #[fg=${PALLETE[bg]},bg=${PALLETE[magenta]},nobold,noitalics,nounderscore] #[fg=${PALLETE[bg]},bg=${PALLETE[magenta]}]#W #F #[fg=${PALLETE[magenta]},bg=${PALLETE[bg]},nobold,noitalics,nounderscore]"
 tmux set -g window-status-separator ""
